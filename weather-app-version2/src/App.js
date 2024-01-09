0b7e20f100abebee2199a4652202e2eab135838b
nik0121 <144052485+nik0121@users.noreply.github.com>
2024-01-09 13:55:33 +0800

    added bootstrap

--- weather-app-version2/src/App.js
+++ weather-app-version2/src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by Nica Guevarra and is <a href="" target="_blank">Open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Nica Guevarra and is{" "}
+          <a
+            href="https://github.com/nik0121/Weather-app-using-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
