4d8709f6ef4df4b9546b9a09f37b83a5aed648b7
nik0121 <144052485+nik0121@users.noreply.github.com>
2024-01-09 13:22:05 +0800

    app set up

---
+++ weather-app-version2/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by Nica Guevarra and is <a href="" target="_blank">Open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
